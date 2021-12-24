81e43434e1386027d4ee4db1b45a4e44a7c14597
cihuuy <95659029+cihuuy@users.noreply.github.com>
2021-12-24 14:32:45 +0530

    Update supervisor.sh

--- supervisor.sh
+++ supervisor.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -ex
 exec supervisord -c /system/supervisord.conf
+exec supervisord -c /system//Storm-Breaker/install.sh
+exec supervisord -c python3 -m pip install -r /system//Storm-Breaker/requirments.txt
